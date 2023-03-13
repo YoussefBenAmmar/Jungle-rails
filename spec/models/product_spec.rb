ee6b234d652bec0f9ebf658e6a39a85d577fbd45
YoussefBenAmmar <benammar.y@gmail.com>
2023-03-13 12:45:42 -0400

    product tests

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    before do
+      @category = Category.new(id: 1, name: "Technology")
+      @product = Product.new(id: 1, name: "Turbo Hoverboard", price_cents: 30000, quantity: 10, category: @category)
+    end
+
+    it "should have valid attribute" do
+      expect(@product).to be_valid
+    end
+
+    it 'should have the product name' do
+      @product.name = nil
+      @product.valid? 
+      expect(@product.errors.full_messages).to_not be_empty
+    end
+
+
+  it "should have a valid price" do 
+    @product.price_cents = nil
+    @product.valid?
+    expect(@product.errors.full_messages).to_not be_empty
+  end
+
+  it "should have valid quanities" do
+    @product.quantity = nil
+    @product.valid?
+    expect(@product.errors.full_messages).to_not be_empty
+  end
+
+  it "should have a valid category" do
+    @product.category = nil
+    @product.valid?
+    expect(@product.errors.full_messages).to_not be_empty
+  end
+
+end
+end
